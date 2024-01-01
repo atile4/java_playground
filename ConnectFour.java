2ae13ba7dbc430263c56142dc5daa3e904a64798
atile <144712135+atile4@users.noreply.github.com>
2023-12-31 20:48:50 -0800

    Update ConnectFour.java
    Replaced some calls to the array with global variables

--- ConnectFour.java
+++ ConnectFour.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 public class ConnectFour {
-    static int LENGTH = 7;
-    static int HEIGHT = 5;
+    public static int LENGTH = 7;
+    public static int HEIGHT = 5;
     static Scanner SCAN = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -15,9 +15,7 @@ public class ConnectFour {
         char playerO = 'O';
         char currPlayer = playerX;
 
-
         int column;
-
 
         while (true) {
             column = getMove(board, currPlayer);
@@ -32,19 +30,21 @@ public class ConnectFour {
 
             if (currPlayer == playerX) { currPlayer = playerO; }
             else { currPlayer = playerX; }
+
         }
     }
 
     public static void resetBoard(char[][] board) {
         for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
+            for (int j = 0; j < HEIGHT; j++) {
                 board[i][j] = ' ';
             }
         }
     }
     public static void printBoard(char[][] a) {
-//        outer: 7 arrays (ABCDEFG)
-//        inner: 5 slots
+//        outer: "LENGTH" arrays (ABCDEFG)
+//        inner: "HEIGHT" slots
+        System.out.println("\n\n\n\n\n");
         System.out.println("  A   B   C   D   E   F   G");
         for (int i = a[0].length-1; i >= 0; i--) {
             System.out.println("|---|---|---|---|---|---|---|");
@@ -65,22 +65,25 @@ public class ConnectFour {
         while (flag) {
             move = SCAN.nextLine();
             if (move.length() == 1) {
-                column = move.charAt(0);
+                column = move.toUpperCase().charAt(0);
                 pos = (int) column - 65;
 
                 if (pos >= 0 && pos <= 6) {
-                    flag = false;
-                    continue;
+                    if (board[pos][HEIGHT-1] == ' ') {
+                        flag = false;
+                        continue;
+                    }
+
                 }
             }
             System.out.print("Please enter a valid input (A, B, C, D, E, F, G): ");
         }
 
-        return pos; //return statement to get intellij to shut up
+        return pos;
     }
 
     public static void setMove(char[][] board, char player, int column) {
-        for (int i = 0; i < board[column].length; i++) {
+        for (int i = 0; i < HEIGHT; i++) {
             if (board[column][i] == ' ') {
                 board[column][i] = player;
                 break;
@@ -89,8 +92,8 @@ public class ConnectFour {
     }
 
     public static boolean checkWin(char[][] board, char player) {
-        for (int row = 0; row < board[0].length/2; row++) {
-            for(int col = 0; col < board.length/2+1; col++) {
+        for (int row = 0; row < HEIGHT/2; row++) {
+            for(int col = 0; col < LENGTH/2+1; col++) {
                 if (board[col][row] == player) {
                     if (player == board[col+1][row] && player == board[col+2][row] &&
                             player == board[col+3][row]) {
@@ -107,7 +110,7 @@ public class ConnectFour {
                 }
             }
 
-            for (int col = board.length/2; col < board.length; col++) {
+            for (int col = LENGTH/2; col < LENGTH; col++) {
                 if (player == board[col][row+1] && player == board[col][row+2] &&
                         player == board[col][row+3]) {
                     return true;
@@ -119,12 +122,8 @@ public class ConnectFour {
 
             }
 
-
         }
 
         return false;
     }
 }
-
-
-
